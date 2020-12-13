2ec4f7c588d2e5ed040ed19d72153f03dd79ee17
duy23092000 <quangduyntt2000@gmail.com>
2020-12-13 22:21:53 +0700

    fix the code structure following the Udacity feedback

--- app.js
+++ app.js
@@ -41,11 +41,10 @@ function getDinoData() {
         );
       });
     });
-    return myDino;
+  return myDino;
 }
 
-
-// 
+//
 // Create Human Object
 function Human(name, feet, inches, weight, diet) {
   this.name = name;
@@ -56,28 +55,15 @@ function Human(name, feet, inches, weight, diet) {
   this.diet = diet;
 }
 
-function createHumanObject() {
-  humanObject = new Human(
-    document.getElementById('name').value,
-    document.getElementById('feet').value,
-    document.getElementById('inches').value,
-    document.getElementById('weight').value,
-    document.getElementById('diet').value,
-  )
-  return humanObject;
-}
-
-
-// Use IIFE to get human data from form
-
-
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
 function compareHeight() {
-  let heightFact = []
+  let heightFact = [];
   for (dino of myDino) {
     let heightGap = parseFloat(dino.height) - parseFloat(humanObject.height);
-    heightFact.push(`The height gap between this dino and the human is ${heightGap} feet`)
+    heightFact.push(
+      `The height gap between this dino and the human is ${heightGap} feet`
+    );
   }
   return heightFact;
 }
@@ -85,10 +71,10 @@ function compareHeight() {
 // Create Dino Compare Method 2
 // NOTE: Weight in JSON file is in lbs, height in inches.
 function compareWeight() {
-  let weightFact = []
+  let weightFact = [];
   for (dino of myDino) {
     let weightGap = parseFloat(dino.weight) - parseFloat(humanObject.weight);
-    weightFact.push(`Comparing by weight, the weight gap is ${weightGap} lbs`)
+    weightFact.push(`Comparing by weight, the weight gap is ${weightGap} lbs`);
   }
   return weightFact;
 }
@@ -99,105 +85,116 @@ function compareDiet() {
   let dietFact = [];
   for (dino of myDino) {
     if (dino.diet === humanObject.diet.toLowerCase()) {
-      dietFact.push(`This dino and the human are in the same ${dino.diet} diet`)
+      dietFact.push(
+        `This dino and the human are in the same ${dino.diet} diet`
+      );
     } else {
-      dietFact.push(`No. They are not in the same diet!`)
+      dietFact.push(`No. They are not in the same diet!`);
     }
   }
   return dietFact;
 }
 
-function randomFact (fact) {
+function randomFact(fact) {
   let keys = Object.keys(fact);
-  return fact[keys[ keys.length * Math.random() << 0]];
-};
+  return fact[keys[(keys.length * Math.random()) << 0]];
+}
 
 function getFact() {
+  compareHeight();
+  compareWeight();
+  compareDiet();
   for (let i = 0; i < 8; i++) {
     let factObject = {};
-    factObject.fact1 = compareHeight()[i]
-    factObject.fact2 = compareWeight()[i]
-    factObject.fact3 = compareDiet()[i]
-    factObject.fact4 = dinoFact[i]
+    factObject.fact1 = compareHeight()[i];
+    factObject.fact2 = compareWeight()[i];
+    factObject.fact3 = compareDiet()[i];
+    factObject.fact4 = dinoFact[i];
     arrayOfFact.push(factObject);
   }
 }
 
-
 // Generate Tiles for each Dino in Array
-function generateTile(dino, factObject) {
-  let grid = document.createElement('div')
-  grid.className = 'grid-item';
-  let avatar = document.createElement('img')
-  avatar.src = "images/" + dino?.species.toLowerCase() + ".png";
-  let name = document.createElement('h3')
-  // name.style.
-  name.innerHTML = dino?.species
-  let fact = document.createElement('p')
-  fact.innerHTML = randomFact(factObject);
-  grid.appendChild(name)
-  grid.appendChild(avatar)
-  grid.appendChild(fact)
+// Abstract function to generate tiles
+function createTilesElement(name, species, fact) {
+  let grid = document.createElement("div");
+  grid.className = "grid-item";
+  let avatar = document.createElement("img");
+  avatar.src = "images/" + species.toLowerCase() + ".png";
+  let speciesName = document.createElement("h3");
+  speciesName.innerHTML = name;
+  grid.appendChild(speciesName);
+  grid.appendChild(avatar);
+
+  if (fact) {
+    let speciesFact = document.createElement("p");
+    speciesFact.innerHTML = fact;
+    grid.appendChild(speciesFact);
+  }
   return grid;
 }
 
+// Function generate dino tiles
+function generateDinoTile(dino, factObject) {
+  const grid = createTilesElement(
+    dino.species,
+    dino.species,
+    randomFact(factObject)
+  );
+  return grid;
+}
+
+// Function generate human tile
 function generateHumanTile(human) {
-  let grid = document.createElement('div')
-  grid.className = 'grid-item';
-  let avatar = document.createElement('img')
-  avatar.src = "images/human.png";
-  let name = document.createElement('h3')
-  name.innerHTML = human.name
-  grid.appendChild(name)
-  grid.appendChild(avatar)
+  const grid = createTilesElement(human.name, "human");
   return grid;
 }
 
+// Function generate pigeon tile
 function generateBirdTile(bird) {
-  let grid = document.createElement('div')
-  grid.className = 'grid-item';
-  let avatar = document.createElement('img')
-  avatar.src = "images/pigeon.png";
-  let name = document.createElement('h3')
-  name.innerHTML = bird?.species
-  let fact = document.createElement('p')
-  fact.innerHTML = arrayOfFact[7]?.fact4;
-  grid.appendChild(name)
-  grid.appendChild(avatar)
-  grid.appendChild(fact)
+  const grid = createTilesElement(
+    "Pigeon",
+    bird.species,
+    arrayOfFact[7]?.fact4
+  );
   return grid;
 }
 
 // Add tiles to DOM
 function renderToDOM() {
   getFact();
-  const mainGrid = document.getElementById('grid')
-  for (let i = j = 0; i < 7; i ++) {
+  const mainGrid = document.getElementById("grid");
+  for (let i = (j = 0); i < 7; i++) {
     if (i == 4) {
-      mainGrid.appendChild(generateHumanTile(humanObject))
+      mainGrid.appendChild(generateHumanTile(humanObject));
     }
-    mainGrid.appendChild(generateTile(myDino[i], arrayOfFact[i]))
+    mainGrid.appendChild(generateDinoTile(myDino[i], arrayOfFact[i]));
   }
-  mainGrid.appendChild(generateBirdTile(myDino[7]))
-  compareHeight();
-  compareWeight();
-  compareDiet();
+  mainGrid.appendChild(generateBirdTile(myDino[7]));
 }
 
 // Remove form from screen
 function removeForm() {
-  const form = document.getElementById('dino-compare')
+  const form = document.getElementById("dino-compare");
   form.remove();
 }
 
 // On button click, prepare and display infographic
 function createButton() {
-  const button = document.getElementById('btn');
-  button.addEventListener('click', () => {
-    createHumanObject(),
-    removeForm(),
-    renderToDOM()
-  })
+  const button = document.getElementById("btn");
+  button.addEventListener("click", () => {
+    // Use IIFE to get human data from form
+    humanObject = (() => {
+      return new Human(
+        document.getElementById("name").value,
+        document.getElementById("feet").value,
+        document.getElementById("inches").value,
+        document.getElementById("weight").value,
+        document.getElementById("diet").value
+      );
+    })();
+    removeForm();
+    renderToDOM();
+  });
 }
-getDinoData(),
-createButton();
+getDinoData(), createButton();
